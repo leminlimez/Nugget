ebcda6f4da2ca754ceda9d5e5db8d336eadf9a41
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-04-03 12:11:22 -0400

    update build number

--- gui/main_window.py
+++ gui/main_window.py
@@ -21,7 +21,7 @@ from tweaks.custom_gestalt_tweaks import CustomGestaltTweaks, ValueTypeStrings
 from tweaks.daemons_tweak import Daemon
 
 App_Version = "5.1"
-App_Build = 7
+App_Build = 0
 
 class Page(Enum):
     Home = 0
