a41d9bb46d510556c1174971d9a0543005383f81
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-06-11 11:35:39 -0400

    fix windows join path for get bundle files

--- controllers/files_handler.py
+++ controllers/files_handler.py
@@ -3,6 +3,6 @@ from os import path, getcwd
 
 def get_bundle_files(name: str):
     try:
-        return path.join(sys._MEIPASS, name)
+        return path.join(sys._MEIPASS, *name.split('/'))
     except:
-        return path.join(getcwd(), name)
+        return path.join(getcwd(), *name.split('/'))
